30a955c1fb7228bc367151c87edccaf4657b5690
A-Barrett <mymajestic1@hotmail.com>
2018-04-08 14:50:23 -0400

    updated

---
+++ app/src/test/java/com/example/android/basketball/MainActivityTest.java
@@ -0,0 +1,97 @@
+package com.example.android.basketball;
+
+import org.junit.Test;
+
+/**
+ * Created by Barrett on 2/18/2018.
+ */
+public class MainActivityTest {
+    @Test
+    public void onCreate1() {
+    }
+
+    @Test
+    public void onSaveInstanceState1() {
+    }
+
+    @Test
+    public void addOneForTeamA1() {
+    }
+
+    @Test
+    public void addTwoForTeamA1() {
+    }
+
+    @Test
+    public void addThreeForTeamA1() {
+    }
+
+    @Test
+    public void resetScore1() {
+    }
+
+    @Test
+    public void displayForTeamA1() {
+    }
+
+    @Test
+    public void addOneForTeamB1() {
+    }
+
+    @Test
+    public void addTwoForTeamB1() {
+    }
+
+    @Test
+    public void addThreeForTeamB1() {
+    }
+
+    @Test
+    public void displayForTeamB1() {
+    }
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void onSaveInstanceState() {
+    }
+
+    @Test
+    public void addOneForTeamA() {
+    }
+
+    @Test
+    public void addTwoForTeamA() {
+    }
+
+    @Test
+    public void addThreeForTeamA() {
+    }
+
+    @Test
+    public void resetScore() {
+    }
+
+    @Test
+    public void displayForTeamA() {
+    }
+
+    @Test
+    public void addOneForTeamB() {
+    }
+
+    @Test
+    public void addTwoForTeamB() {
+    }
+
+    @Test
+    public void addThreeForTeamB() {
+    }
+
+    @Test
+    public void displayForTeamB() {
+    }
+
+}
